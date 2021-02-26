b26032892c06bcdc8229ab68e4c6dd708fb651fe
masterhand83 <robinmiguelsdj@gmail.com>
2021-02-26 17:14:55 -0600

    added view scripts

--- GUI/main_view.gd
+++ GUI/main_view.gd
@@ -1,2 +1,14 @@
 extends Control
 onready var current_view = $MainMenu
+
+func change_view(view: String):
+    if(view == 'editor'):
+        $EditorMenu.show()
+        $MainMenu.hide()
+    else:
+        $EditorMenu.hide()
+        $MainMenu.show()
+
+
+
+
