99f2733f8307da1b538690fd96b5474a1784c314
masterhand83 <robinmiguelsdj@gmail.com>
2021-02-27 17:35:42 -0600

    added next/choice toggler

--- dialog_node/base_dialog_node.gd
+++ dialog_node/base_dialog_node.gd
@@ -19,6 +19,9 @@ func _on_ChoiceMenu_add_choice() -> void:
     line_edit.placeholder_text = 'choice'
     choice_inputs[str(choice_index)] = line_edit
     add_child(line_edit, true)
+    set_slot(choice_index,
+            false, 0, DEFAULT_COLOR,
+            true, DEFAULT_PORT, DEFAULT_COLOR)
     choice_index += 1
     update()
     force_update_transform()
@@ -28,11 +31,25 @@ func _on_ChoiceMenu_delete_choice() -> void:
     var last_index = get_child_count() - 1;
     if last_index <= CHOICE_MENU_INDEX:
         return
-    set_slot(last_index,
-            false, 0, DEFAULT_COLOR,
-            false, DEFAULT_PORT, DEFAULT_COLOR)
     var choice_input = choice_inputs.get(str(last_index))
     choice_inputs.erase(str(last_index))
     choice_input.queue_free()
+    clear_slot(last_index)
     choice_index -= 1
     rect_size.y = 0
+
+
+func _on_CheckBox_toggled(button_pressed: bool) -> void:
+    $NextLabel.visible = not button_pressed
+    $ChoiceMenu.visible = button_pressed
+    set_slot(NEXT_LABEL_INDEX,
+            false, 0, DEFAULT_COLOR,
+            not button_pressed, DEFAULT_PORT, DEFAULT_COLOR)
+    for index in choice_inputs.keys():
+        var i = int(index)
+        set_slot(i,
+                false, 0, DEFAULT_COLOR,
+                button_pressed, DEFAULT_PORT, DEFAULT_COLOR)
+        choice_inputs[index].visible = button_pressed
+
+
