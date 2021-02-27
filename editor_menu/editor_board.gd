09f48076aedd09c0e80aee3d292d358e5b3a7892
masterhand83 <robinmiguelsdj@gmail.com>
2021-02-27 14:47:42 -0600

    board is able to add graph_nodes

--- editor_menu/editor_board.gd
+++ editor_menu/editor_board.gd
@@ -1,1 +1,6 @@
 extends GraphEdit
+onready var DialogNodeResource= preload('res://dialog_node/dialog_node.tscn')
+
+func add_dialog():
+    var dialog_node = DialogNodeResource.instance()
+    add_child(dialog_node, true)
--- editor_menu/editor_menu.gd
+++ editor_menu/editor_menu.gd
@@ -5,3 +5,7 @@ extends View
 
 func _on_EditorButtons_return_to_menu() -> void:
     emit_signal('change_view', 'main')
+
+
+func _on_EditorButtons_add_dialog() -> void:
+    $EditorBoard.add_dialog()
