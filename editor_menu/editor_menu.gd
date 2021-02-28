cfb4e384ae9dab0c95c64673dc8299f29b1ecdd9
masterhand83 <robinmiguelsdj@gmail.com>
2021-02-27 18:50:23 -0600

    conected board with buttons

--- editor_menu/editor_menu.gd
+++ editor_menu/editor_menu.gd
@@ -1,11 +1,17 @@
 extends View
 
 
-
-
 func _on_EditorButtons_return_to_menu() -> void:
     emit_signal('change_view', 'main')
 
 
 func _on_EditorButtons_add_dialog() -> void:
     $EditorBoard.add_dialog()
+
+
+func _on_EditorButtons_add_start_dialog() -> void:
+    $EditorBoard.add_start_dialog()
+
+
+func _on_EditorButtons_add_final_dialog() -> void:
+    $EditorBoard.add_final_dialog()
