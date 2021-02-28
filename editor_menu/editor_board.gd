ecf14ad648b74fa8c4e0d58e070f83fa8ca5e0c5
masterhand83 <robinmiguelsdj@gmail.com>
2021-02-27 18:53:45 -0600

    adding board features

--- editor_menu/editor_board.gd
+++ editor_menu/editor_board.gd
@@ -24,3 +24,11 @@ func _on_EditorBoard_connection_request(
         to: String,
         to_slot: int) -> void:
     connect_node(from, from_slot, to, to_slot)
+
+
+func _on_EditorBoard_disconnection_request(
+        from: String,
+        from_slot: int,
+        to: String,
+        to_slot: int) -> void:
+    disconnect_node(from, from_slot, to, to_slot)
